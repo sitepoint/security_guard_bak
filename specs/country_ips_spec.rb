e5007a26cc5f3b29ab1bc645704b7f39dd558bef
Fred Wu <ifredwu@gmail.com>
2012-02-11 15:47:39 +1100

    use let blocks in specs

--- specs/country_ips_spec.rb
+++ specs/country_ips_spec.rb
@@ -1,18 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe SecurityGuard::CountryIps do
-  before do
-    @country_ips = SecurityGuard::CountryIps.new(
+  let(:country_ips) do
+    SecurityGuard::CountryIps.new(
       :countries => ['Australia'],
       :ips       => ['4.4.4.4', '8.8.8.8', '203.206.0.1']
     )
   end
 
   it 'contains GeoIP data' do
-    @country_ips.instance_variable_get(:@geoip).must_be_instance_of SecurityGuard::Utils::GeoIps
+    country_ips.instance_variable_get(:@geoip).must_be_instance_of SecurityGuard::Utils::GeoIps
   end
 
   it 'returns all IPs from the given country and IP dictionaries' do
-    @country_ips.result.must_equal ['203.206.0.1']
+    country_ips.result.must_equal ['203.206.0.1']
   end
 end
